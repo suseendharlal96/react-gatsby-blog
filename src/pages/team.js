257e4c05519b11390e64457e34c29c835982bcac
unknown <lssuseendharlal@gmail.com>
2020-07-29 18:39:38 +0530

    pagination added

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,13 +1,48 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const Team = () => (
   <Layout>
     <Seo title="Team" />
     <h2>Team Page</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ut nemo
+      neque. Officiis vel nostrum maxime, doloremque magni nihil. Et iste at
+      repellendus asperiores doloremque molestias velit assumenda ullam beatae
+      voluptates dolore unde impedit architecto numquam, ut quam enim. In,
+      distinctio quos repellat natus officiis quidem commodi esse? Officia animi
+      eius incidunt cum aperiam obcaecati quae illum, laborum maxime labore
+      inventore, fuga, accusamus neque ducimus fugiat perspiciatis nemo nam
+      libero quaerat error consequatur debitis rem minus tempore. Sapiente,
+      repudiandae ad? Iusto quibusdam maxime officiis voluptates! Enim quos
+      repellat mollitia ex, ullam iusto amet aliquid eius quod, suscipit maxime
+      aperiam rem fugit voluptas, atque tenetur dolorem. Labore numquam
+      voluptatem atque eligendi, odit molestias delectus itaque quia sequi
+      perspiciatis quidem illo accusantium neque dolore iusto placeat facere
+      nemo distinctio voluptas laborum est ipsam consectetur quam? Officiis,
+      saepe dolore nobis, accusamus nisi cupiditate modi itaque corrupti
+      veritatis obcaecati, iure repudiandae! Dolore voluptatibus nobis ratione
+      quas corrupti culpa, dolores consequatur fugit eum quaerat aperiam
+      inventore incidunt laborum est iure reprehenderit natus esse quo
+      laudantium nemo impedit ab ea ad architecto. Ex blanditiis dolorum facere
+      perspiciatis dolores quis ipsum soluta aliquid laborum aperiam. Aspernatur
+      saepe numquam dicta consequuntur, nam nemo accusamus laborum omnis,
+      cupiditate placeat rerum voluptatibus dolorem corporis molestias.
+      Exercitationem, praesentium. Dolorum, nisi praesentium iste maxime ad rem,
+      nesciunt a cupiditate, dignissimos iure minus sed quod mollitia autem modi
+      corrupti quidem accusamus voluptates itaque odio optio? Qui iure sequi
+      amet! Quos itaque tempore magnam? Adipisci explicabo assumenda modi
+      obcaecati eius quasi necessitatibus rem aspernatur pariatur repudiandae
+      temporibus ut tempore unde hic, reiciendis accusamus expedita! Beatae
+      error iste mollitia deserunt recusandae perspiciatis, alias vel blanditiis
+      ipsa provident aperiam, corporis rem! Suscipit assumenda voluptate illum
+      eveniet natus ullam sed adipisci consequatur. Neque officia, doloremque,
+      cumque ut placeat eveniet reiciendis non fugit enim debitis nisi porro
+      ipsam.
+    </p>
   </Layout>
-)
+);
 
-export default Team
+export default Team;
